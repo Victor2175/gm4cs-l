7f7bc5987110ace90cabfe55b7e825b03f3d4d54
lharriso <100597379+lharriso@users.noreply.github.com>
2025-03-15 19:27:46 +0100

    removed extra useless lines

--- utils/helpers.py
+++ utils/helpers.py
@@ -3,8 +3,6 @@ import pickle as pkl
 import numpy as np
 import random
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.decomposition import TruncatedSVD
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 import seaborn as sns
